f435c16d71472c9d134974c75d9fa4a064727be6
ross <ross@mini.local>
2019-11-09 11:19:45 -0800

    Did more work.

--- gradebook/Program.cs
+++ gradebook/Program.cs
@@ -12,27 +12,43 @@ namespace gradebook
     {
         static void Main(string[] args)
         {
+            // Example of a basic variable initialization.
+            // in this case we are saying the var `x` has a type of `double`.
+            double x;
+            x = 34.1;
+            // Single line form of basic initialization.
+            double y = 10.3;
             // Here a new variable is being initialized using the var
-            // key word. The could of also been written in the explicit:
-            // 
-            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var addReturn = x + y;
+            Console.WriteLine(addReturn);
+            
+            /* Initializing a new Array.
+            The key word `new` is part of how an Array gets Initialized.
+            In this example a new Array is created with an explicit size
+            of four and four values are provided*/ 
+            var numbers = new double[4] { 12.7, 10.3, 6.11, 4.1 };
 
-            // List type is not found in the System namespace.
-            // Select the word `List` and then hit `command+.` (macOS)
-            // and vscode will bring up a menu of possible namespaces where
-            // `List` is stored.
+
+            /* List type is not found in the System namespace.
+            Select the word `List` and then hit `command+.` (macOS)
+            and vscode will bring up a menu of possible namespaces where
+            `List` is stored. Can be expicity written as:
+            `List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };`*/
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             // With the List type we can add values to it.
             // Unlike Arrays that once they are setup become immutible.
             grades.Add(56.1);
 
             var result = 0.0;
-            foreach (var number in numbers)
+            foreach (var number in grades)
             {
                 // Shortcut way of take the var and then add it to itself.
                 result += number;
             }
-            Console.WriteLine(result);
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result}.");
+            // A way to format the number.
+            Console.WriteLine($"The average grade formatted {result:N3}.");
 
             // Example of a `if-else` control.
             if (args.Length > 0)
